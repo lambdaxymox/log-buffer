7cef8431fc242106776cf50dfc61e8b1fa98fca3
LambdaXymox <lambda.xymox@gmail.com>
2018-12-30 15:47:11 -0600

    Add test.

--- tests/log_buffer.rs
+++ tests/log_buffer.rs
@@ -14,6 +14,7 @@ fn empty_log_buffer_should_be_empty() {
 #[test]
 fn empty_log_buffer_extract_should_be_empty_string() {
     let mut log_buffer = LogBuffer::new([0x00; 16]);
+
     let result = log_buffer.extract();
     let expected = "";
 
@@ -39,10 +40,10 @@ fn log_buffer_should_have_capacity_equal_to_underlying_storage_size() {
 #[test]
 fn log_buffer_extracted_string_should_match_inserted_string_of_length_at_most_the_buffer_size() {
     let mut log_buffer = LogBuffer::new([0x00; 16]);
-    let expected = "abcdefghijklmnop";
-    write!(log_buffer, "{}", expected).unwrap();
-    eprintln!("{:?}", log_buffer);
+    write!(log_buffer, "abcdefghijklmnop").unwrap();
+
     let result = log_buffer.extract();
+    let expected = "abcdefghijklmnop";
 
     assert_eq!(result, expected);
 }
@@ -67,6 +68,21 @@ fn log_buffer_containing_data_should_not_be_empty() {
 fn log_buffer_string_longer_than_buffer_length_should_wrap_around() {
     let mut log_buffer = LogBuffer::new([0x00; 16]);
     write!(log_buffer, "abcdefghijklmnopqrstuv").unwrap();
+
+    let expected = "ghijklmnopqrstuv";
+    let result = log_buffer.extract();
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn log_buffer_should_correctly_extract_data_after_multiple_cycles() {
+    let mut log_buffer = LogBuffer::new([0x00; 16]);
+    write!(log_buffer, "abcdefghijklmnop").unwrap();
+    write!(log_buffer, "abcdefghijklmnop").unwrap();
+    write!(log_buffer, "abcdefghijklmnop").unwrap();
+    write!(log_buffer, "abcdefghijklmnopqrstuv").unwrap();
+
     let expected = "ghijklmnopqrstuv";
     let result = log_buffer.extract();
 
