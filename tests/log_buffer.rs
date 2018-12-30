aec8d27c07d177e8c5807c72e6830d14ab7e08a2
LambdaXymox <lambda.xymox@gmail.com>
2018-12-30 02:31:39 -0600

    Remove unneeded mutable attributes.

--- tests/log_buffer.rs
+++ tests/log_buffer.rs
@@ -5,14 +5,14 @@ use std::fmt::Write;
 
 
 #[test]
-fn test_empty_log_buffer_should_be_empty() {
+fn empty_log_buffer_should_be_empty() {
     let log_buffer = LogBuffer::new([0x00; 16]);
 
     assert!(log_buffer.is_empty());
 }
 
 #[test]
-fn test_empty_log_buffer_extract_should_be_empty_string() {
+fn empty_log_buffer_extract_should_be_empty_string() {
     let mut log_buffer = LogBuffer::new([0x00; 16]);
     let result = log_buffer.extract();
     let expected = "";
@@ -22,16 +22,26 @@ fn test_empty_log_buffer_extract_should_be_empty_string() {
 }
 
 #[test]
-fn test_empty_log_buffer_should_have_length_zero() {
-    let mut log_buffer = LogBuffer::new([0x00; 16]);
+fn empty_log_buffer_should_have_length_zero() {
+    let log_buffer = LogBuffer::new([0x00; 16]);
 
     assert_eq!(log_buffer.len(), 0);
 }
 
 #[test]
-fn test_log_buffer_should_have_capacity_equal_to_underlying_storage_size() {
-    let mut storage = [0xFF as u8; 32];
-    let mut log_buffer = LogBuffer::new(storage);
+fn log_buffer_should_have_capacity_equal_to_underlying_storage_size() {
+    let storage = [0xFF as u8; 32];
+    let log_buffer = LogBuffer::new(storage);
 
     assert_eq!(log_buffer.capacity(), storage.len());
+}
+
+#[test]
+fn log_buffer_extracted_string_should_match_inserted_string() {
+    let mut log_buffer = LogBuffer::new([0x00; 16]);
+    let expected = "abcdefghijklmnop";
+    write!(log_buffer, "{}", expected).unwrap();
+    let result = log_buffer.extract();
+
+    assert_eq!(result, expected);
 }
